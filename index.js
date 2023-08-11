b31c3ec6ba380ff8fb030f17c87967e8d79b0ce4
Pratiksha Ghadge <85682428+pratiksha23-github@users.noreply.github.com>
2023-08-11 16:00:13 +0530

    updated package.json & index.js files for deployement

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const PORT = 4000;
+const port = 4000;
 const app = express();
 const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
@@ -12,15 +12,12 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 
-
-
-
 //routes
 app.use('/', require('./routes/csvRoutes'));
 
 //server listening
-app.listen(PORT, (err) => {
-    if(err) console.log("error listening on", PORT);
+app.listen(process.env.PORT||port, (err) => {
+    if(err) console.log("error listening on", port);
 
-    console.log('listening on port', PORT);
+    console.log('listening on port', port);
 })
